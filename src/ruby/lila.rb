a3d243109c5f50452fc3001dbf44618a3b667e12
Bastian Mueller <bastian@turbolent.com>
2013-03-09 17:27:57 +0100

    interpreter: initialize runtime at startup

--- src/ruby/lila.rb
+++ src/ruby/lila.rb
@@ -1,5 +1,8 @@
 (puts("Usage: lila <path>"); exit) if ARGV.empty?
 
 require 'lila/interpreter'
+require 'java'
+java_import 'lila.runtime.RT'
 
+RT.initialize
 Lila::Interpreter.new.run_file(ARGV.first)
